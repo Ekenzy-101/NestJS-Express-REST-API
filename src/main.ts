1bd0ad05a9ddef37075770ab9ca0ef2baade9c8f
Ekenzy-101 <ekeneonyekaba@gmail.com>
2021-03-22 08:02:34 +0100

    Initial setup

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const port = process.env.PORT || 5000;
+  await app.listen(port);
+}
+bootstrap();
